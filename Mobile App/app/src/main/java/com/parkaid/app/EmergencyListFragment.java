ec26fca5a1e18fbcf3de1287d097fe343eae7947
Abhishek Sisodia <abhisheksisodia13@gmail.com>
2015-08-27 22:08:33 -0400

    Added functionality to pick contacts from phone contact list

--- Mobile App/app/src/main/java/com/parkaid/app/EmergencyListFragment.java
+++ Mobile App/app/src/main/java/com/parkaid/app/EmergencyListFragment.java
@@ -1,10 +1,14 @@
 package com.parkaid.app;
 
 import android.app.Fragment;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,68 +18,44 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import com.google.gson.Gson;
 
+import com.google.gson.reflect.TypeToken;
 import com.parkaid.app.adapter.UserAdapter;
 import com.parkaid.app.model.User;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class EmergencyListFragment extends Fragment {
 
 	public EmergencyListFragment(){}
     private Uri uriContact;
-    private String contactID;     // contacts unique ID
-    public ArrayList<String> list;
+    public Gson gson;
+    public ArrayList<User> arrayOfUsers;
     public ListView listview;
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
 
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         ViewGroup mainView = (ViewGroup) inflater.inflate(R.layout.fragment_emergency, container, false);
 
-        // Construct the data source
-        ArrayList<User> arrayOfUsers = new ArrayList<User>();
-        // Create the adapter to convert the array to views
-        UserAdapter adapter = new UserAdapter(getActivity(), arrayOfUsers);
-        // Attach the adapter to a ListView
+        SharedPreferences appSharedPrefs = PreferenceManager
+                .getDefaultSharedPreferences(getActivity().getApplicationContext());
+        Gson gson = new Gson();
+        String contacts = appSharedPrefs.getString("EmergencyContactList", "");
+        if (contacts.isEmpty()){
+            arrayOfUsers = new ArrayList<User>();
+        } else {
+            Type type = new TypeToken<List<User>>(){}.getType();
+            arrayOfUsers = gson.fromJson(contacts, type);
+        }
+
         listview = (ListView) mainView.findViewById(R.id.ListView);
-        listview.setAdapter(adapter);
-
-        adapter.add(new User("Drupadh Manjunath", "151-181-1919"));
-        adapter.add(new User("Rajit Venkatesh", "151-181-1919"));
-        adapter.add(new User("Irteza Arif", "151-181-1919"));
-        adapter.add(new User("Ali", "151-181-1919"));
-        adapter.add(new User("Abhishek", "151-181-1919"));
-
         return mainView;
     }
-
-    private class StableArrayAdapter extends ArrayAdapter<String> {
-
-        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
-        public StableArrayAdapter(Context context, int textViewResourceId,
-                                  List<String> objects) {
-            super(context, textViewResourceId, objects);
-            for (int i = 0; i < objects.size(); ++i) {
-                mIdMap.put(objects.get(i), i);
-            }
-        }
-
-        @Override
-        public long getItemId(int position) {
-            String item = getItem(position);
-            return mIdMap.get(item);
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-    }
-
+    
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
@@ -89,43 +69,45 @@ public class EmergencyListFragment extends Fragment {
 
         @Override
         public boolean onMenuItemClick(MenuItem item) {
-
-            startActivityForResult(new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI), 1);
+            Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+            pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+            startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
             return true;
         }
     };
 
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == 1 && resultCode == getActivity().RESULT_OK) {
-//            uriContact = data.getData();
-//
-//            ContentResolver cr = getActivity().getContentResolver();
-//            Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-//
-//            if (cur.getCount() > 0) {
-//                while (cur.moveToNext()) {
-//                    String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
-//                    String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-//                    if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-//                        // This inner cursor is for contacts that have multiple numbers.
-//                        Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[] { id }, null);
-//                        pCur.close();
-//                    }
-//                }
-//                Collections.sort(list);
-//
-//                final StableArrayAdapter adapter = new StableArrayAdapter(getActivity(),
-//                        android.R.layout.simple_list_item_1, list);
-//                listview.setAdapter(adapter);
-//                listview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-//
-//            }
-//            cur.close();
-//        }
-//    }
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        // Check which request it is that we're responding to
+        if (reqCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == getActivity().RESULT_OK) {
+                // Get the URI that points to the selected contact
+                Uri contactUri = data.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER,ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
+                Cursor cursor = getActivity().getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int phoneNumColumn = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                int nameColumn = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                String number = cursor.getString(phoneNumColumn);
+                String name = cursor.getString(nameColumn);
+                arrayOfUsers.add(new User(name, number));
+            }
+        }
+        UserAdapter adapter = new UserAdapter(getActivity(), arrayOfUsers);
+        listview.setAdapter(adapter);
+        gson = new Gson();
+        String emergencyContacts = gson.toJson(arrayOfUsers);
+        SharedPreferences appSharedPrefs = PreferenceManager
+                .getDefaultSharedPreferences(getActivity().getApplicationContext());
+        SharedPreferences.Editor prefsEditor = appSharedPrefs.edit();
+        prefsEditor.putString("EmergencyContactList", emergencyContacts);
+        prefsEditor.commit();
+    }
 }
 
 
